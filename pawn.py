1a95b9271d4a027084e54851f5748516ac669a2e
Kimi Löffel <kimi.loeffel@hispeed.ch>
2021-08-20 16:01:08 +0200

    update

--- pawn.py
+++ pawn.py
@@ -4,3 +4,19 @@ class Pawn:
         self.char = "P"
         self.pos = [xPos, yPos]
         self.color = color
+        self.availableMoves = []
+
+        def isAvailable(self, x, y, chessboard):
+            if x != -1 and y != -1 and y != len(chessboard) and x != len(chessboard[0]):
+                if chessboard[y][x] == " ":
+                    return True
+                else:
+                    return False
+            else:
+                return False
+
+        def isAvailableOpponent(self, x, y, chessboard):
+            if chessboard[y][x] == " ":
+                return True
+            else:
+                return False
