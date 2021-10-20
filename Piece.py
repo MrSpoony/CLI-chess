37805dd49941095f90cd36b44dce7dbde277f2ae
Kimi Löffel <kimi.loeffel@hispeed.ch>
2021-10-20 13:17:01 +0200

    New Class Piece, which controlls overall functions

---
+++ Piece.py
@@ -0,0 +1,34 @@
+class Piece:
+
+    def __init__(self, xPos, yPos, color):
+        self.pos = [xPos, yPos]
+        self.color = color
+
+        # so you know which color it is, there is a "W" in front if it's a piece of white and the opposite with a "B" for black 
+        if self.color:
+            self.char = "W"
+        else:
+            self.char = "B"
+
+    def isAvailable(self, x, y, chessboard):
+        if not(x <= -1 or y <= -1 or y >= len(chessboard) or x >= len(chessboard[0])):
+            if chessboard[y][x] == " ":
+                return True
+            else:
+                return False
+        else:
+            return False
+
+            
+    def isAvailableOpponent(self, x, y, chessboard):
+        if chessboard[y][x] == " ":
+            return True
+        else:
+            return False
+
+    def setNewCoordinates(self, chessboard):
+        for i in range(len(chessboard[int(self.color)])):
+            for j in range(len(chessboard[int(self.color)][i])):
+                if chessboard[int(self.color)][i][j] != " ":
+                    if chessboard[int(self.color)][i][j].pos == self.pos:
+                        self.pos = [j, i]
--- king.py
+++ king.py
@@ -1,40 +1,10 @@
-class King:
+from Piece import Piece
+class King(Piece):
 
     def __init__(self, xPos, yPos, color):
-        self.char = "K"
-        self.pos = [xPos, yPos]
-        self.color = color
-
-        # so you know which color it is, there is a "W" in front if it's a piece of white and the opposite with a "B" for black 
-        if self.color:
-            self.char = "W" + self.char
-        else:
-            self.char = "B" + self.char
-
+        super().__init__(xPos, yPos, color)
+        self.char += "K"
         self.availableMoves = [self.char]
-
-    def isAvailable(self, x, y, chessboard):
-        if not(x <= -1 or y <= -1 or y >= len(chessboard) or x >= len(chessboard[0])):
-            if chessboard[y][x] == " ":
-                return True
-            else:
-                return False
-        else:
-            return False
-
-    def isAvailableOpponent(self, x, y, chessboard):
-        if chessboard[y][x] == " ":
-            return True
-        else:
-            return False
-
-    def setNewCoordinates(self, chessboard):
-        for i in range(len(chessboard[int(self.color)])):
-            for j in range(len(chessboard[int(self.color)][i])):
-                if chessboard[int(self.color)][i][j] != " ":
-                    if chessboard[int(self.color)][i][j].pos == self.pos:
-                        self.pos = [j, i]
-
 
     def checkForAvailableMoves(self, chessboard):
         moveOffsetOptions = [-1, 0, 1]
