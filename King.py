73fb218ee402dfa0793f1d98bbce072c7cd3ba63
MrSpoony <kimi.loeffel@hispeed.ch>
2022-01-21 22:50:49 +0100

    Just some formatting and spelling correcitons.

--- King.py
+++ King.py
@@ -1,43 +1,51 @@
 from Piece import Piece
 from Rook import Rook
 import colorama
+
+
 class King(Piece):
 
     def __init__(self, xPos, yPos, color):
-        '''
-        New King Piece at position xPos, yPos, and the color color'''
+        """
+        New King Piece at position xPos, yPos, and the color"""
         super().__init__(xPos, yPos, color)
         self.char += "K" + colorama.Style.RESET_ALL
 
     def __str__(self):
-        '''
-        Sets the Sring of the Object to it's char so I can access it with str()'''
+        """
+        Sets the String of the Object to its char, so I can access it with str()"""
         return self.char
 
     def checkForAvailableMoves(self, chessboard):
-        '''
-        Checks for the available moves from this piece'''
+        """
+        Checks for the available moves from this piece"""
         self.availableMoves = []
         moveOffsetOptions = [-1, 0, 1]
         for i in range(len(moveOffsetOptions)):
             for j in range(len(moveOffsetOptions)):
                 currentOffsetX = moveOffsetOptions[i]
                 currentOffsetY = moveOffsetOptions[j]
-                if self.isAvailable(self.pos[0] + currentOffsetX, self.pos[1] + currentOffsetY, chessboard[int(self.color)]):
-                    self.availableMoves.append([[self.pos[0], self.pos[1]], [self.pos[0]  + currentOffsetX, self.pos[1] + currentOffsetY]])
+                if self.isAvailable(self.pos[0] + currentOffsetX, self.pos[1] + currentOffsetY,
+                                    chessboard[int(self.color)]):
+                    self.availableMoves.append(
+                        [[self.pos[0], self.pos[1]], [self.pos[0] + currentOffsetX, self.pos[1] + currentOffsetY]])
         self.checkForRochade(chessboard)
         return self.availableMoves
 
     def checkForRochade(self, chessboard):
-        '''
+        """
         Checks if there is a rochade possible/available
-        if yes then it appends the rochade moves to self.availableMoves'''
+        if yes then it appends the rochade moves to self.availableMoves"""
         if not self.moved:
             if self.color:
                 if type(chessboard[int(self.color)][7][0]) is Rook:
                     if not chessboard[int(self.color)][7][0].moved:
-                        if chessboard[int(self.color)][7][1] == " " and chessboard[int(self.color)][7][2] == " " and chessboard[int(self.color)][7][3] == " ":
-                            if chessboard[int(not self.color)][7][1] == " " and chessboard[int(not self.color)][7][2] == " " and chessboard[int(not self.color)][7][3] == " ":
+                        if chessboard[int(self.color)][7][1] == " " and \
+                                chessboard[int(self.color)][7][2] == " " and \
+                                chessboard[int(self.color)][7][3] == " ":
+                            if chessboard[int(not self.color)][7][1] == " " and \
+                                    chessboard[int(not self.color)][7][2] == " " and \
+                                    chessboard[int(not self.color)][7][3] == " ":
                                 self.availableMoves.append([[self.pos[0], self.pos[1]], [self.pos[0] - 2, self.pos[1]]])
                 if type(chessboard[int(self.color)][7][7]) is Rook:
                     if not chessboard[int(self.color)][7][7].moved:
@@ -47,11 +55,16 @@ class King(Piece):
             else:
                 if type(chessboard[int(self.color)][0][0]) is Rook:
                     if not chessboard[int(self.color)][0][0].moved:
-                        if chessboard[int(self.color)][0][1] == " " and chessboard[int(self.color)][0][2] == " " and chessboard[int(self.color)][0][3] == " ":
-                            if chessboard[int(not self.color)][0][1] == " " and chessboard[int(not self.color)][0][2] == " " and chessboard[int(not self.color)][0][3] == " ":
+                        if chessboard[int(self.color)][0][1] == " " and \
+                                chessboard[int(self.color)][0][2] == " " and \
+                                chessboard[int(self.color)][0][3] == " ":
+                            if chessboard[int(not self.color)][0][1] == " " and \
+                                    chessboard[int(not self.color)][0][2] == " " and \
+                                    chessboard[int(not self.color)][0][3] == " ":
                                 self.availableMoves.append([[self.pos[0], self.pos[1]], [self.pos[0] - 2, self.pos[1]]])
                 if type(chessboard[int(self.color)][0][7]) is Rook:
                     if not chessboard[int(self.color)][0][7].moved:
                         if chessboard[int(self.color)][0][6] == " " and chessboard[int(self.color)][0][5] == " ":
-                            if chessboard[int(not self.color)][0][6] == " " and chessboard[int(not self.color)][0][5]:
+                            if chessboard[int(not self.color)][0][6] == " " and \
+                                    chessboard[int(not self.color)][0][5]:
                                 self.availableMoves.append([[self.pos[0], self.pos[1]], [self.pos[0] + 2, self.pos[1]]])
--- Piece.py
+++ Piece.py
@@ -1,14 +1,16 @@
 import colorama
+
+
 class Piece:
 
     def __init__(self, xPos, yPos, color):
-        '''
+        """
         Initiates the new piece with the variables
         self.pos => position in array [x, y]
-        selt.color => color as bool
+        self.color => color as bool
         self.availableMoves => [] empty at beginning but gets populated with moves later in function CheckForAvailableMoves
         self.moved => False at the beginning gets changed to true when moved
-        self.char => W/B at the beginning gets changed to WK BQ etc. in the specific piece Classes, W for white B for black'''
+        self.char => W/B at the beginning gets changed to WK BQ etc. in the specific piece Classes, W for white B for black"""
         self.pos = [xPos, yPos]
         self.color = color
         self.availableMoves = []
@@ -18,10 +20,11 @@ class Piece:
         else:
             self.char = colorama.Fore.RED + "B"
 
-    def isAvailable(self, x, y, chessboard):
-        '''
-        Checks if position x, y on the chessboard of your own color is available'''
-        if not(x <= -1 or y <= -1 or y >= len(chessboard) or x >= len(chessboard[0])):
+    @staticmethod
+    def isAvailable(x, y, chessboard):
+        """
+        Checks if position x, y on the chessboard of your own color is available"""
+        if not (x <= -1 or y <= -1 or y >= len(chessboard) or x >= len(chessboard[0])):
             if chessboard[y][x] == " ":
                 return True
             else:
@@ -30,9 +33,9 @@ class Piece:
             return False
 
     def setNewCoordinates(self, chessboard):
-        '''
+        """
         Resets all the coordinates
-        Loops through every piece checks it's position and writes that position to its self.pos variable'''
+        Loops through every piece checks it's position and writes that position to its self.pos variable"""
         for i in range(len(chessboard[int(self.color)])):
             for j in range(len(chessboard[int(self.color)][i])):
                 if chessboard[int(self.color)][i][j] != " ":
